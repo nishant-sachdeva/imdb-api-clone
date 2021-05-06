a7f85b6795a46640617d597b0665c94324c80be2
nishant-sachdeva <nishant.sachdeva@research.iiit.ac.in>
2021-05-06 20:04:19 +0530

    basic users added

--- imdb/watchlist/api/views.py
+++ imdb/watchlist/api/views.py
@@ -1,7 +1,7 @@
 from rest_framework.response  import Response
 
 from django.shortcuts import get_object_or_404
-
+from rest_framework.exceptions import ValidationError
 from rest_framework.views import APIView 
 from rest_framework import generics
 from rest_framework import viewsets
@@ -17,11 +17,21 @@ class ReviewCreate(generics.CreateAPIView):
 	# we need to overwrite the current function
 	serializer_class = ReviewSerializer
 
+	def get_queryset(self):
+		return Review.objects.all()
+
 	def perform_create(self, serializer):
 		pk = self.kwargs.get('pk')
-		movie = WatchList.objects.get(pk = pk)
+		queryset = WatchList.objects.all()
+		watchlist = get_object_or_404(queryset, pk=pk)
 
-		serializer.save(watchlist=movie)
+		review_user = self.request.user 
+		review_queryset = Review.objects.filter(watchlist=watchlist, review_user=review_user)
+
+		if review_queryset.exists():
+			raise ValidationError("Can't comment on same movie more than once!")
+
+		serializer.save(watchlist=watchlist, review_user=review_user)
 
 class ReviewList(generics.ListCreateAPIView):
 	# queryset = Review.objects.all()
@@ -37,7 +47,7 @@ class ReviewDetail(generics.RetrieveUpdateDestroyAPIView):
 	serializer_class = ReviewSerializer
 
 
-class StreamPlatformViewSet(viewsets.ReadOnlyModelViewSet):
+class StreamPlatformViewSet(viewsets.ModelViewSet):
 	queryset = StreamPlatform.objects.all()
 	serializer_class = StreamPlatformSerializer
 
--- imdb/watchlist/models.py
+++ imdb/watchlist/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
 
+from django.contrib.auth.models import User 
 # Create your models here.
 
 class StreamPlatform(models.Model):
@@ -24,6 +25,7 @@ class WatchList(models.Model):
 
 
 class Review(models.Model):
+	review_user = models.ForeignKey(User, on_delete=models.CASCADE)
 	rating = models.PositiveIntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
 	description = models.CharField(max_length=200, null=True)
 	created = models.DateTimeField(auto_now_add=True)
